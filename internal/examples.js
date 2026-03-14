6ef1d6412b2af2950d822ce0ae774c243cb2591e
Morris L <morrislli@outlook>
2026-03-13 19:21:45 -0700

    example uploaded

---
+++ internal/examples.js
@@ -0,0 +1,22 @@
+import { LeetCode } from '@leetnotion/leetcode-api';
+
+const entrypoint = new LeetCode();
+
+// Example filter, everything is optional
+const problemFilter = {
+    filters: {
+
+        // EASY | MEDIUM | HARD
+        difficulty: "EASY"
+    },
+    limit: 10
+}
+
+// Get problems
+const p = entrypoint.problems(problemFilter);
+
+p.then((plist) => {
+    for (const prob of plist.questions) {
+        console.log(`Problem title: ${prob.title}, AC rate: ${prob.acRate}`);
+    }
+});
