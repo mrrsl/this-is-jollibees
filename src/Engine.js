9044189f4d5a8c3d767fd57198e136fb8d7dad0b
SadRectangle <graceyang803@gmail.com>
2026-03-14 12:35:28 -0700

    changed it so when you import a problem it creates a folder with the problem name and the solution.js inside the folder

--- src/Engine.js
+++ src/Engine.js
@@ -15,115 +15,141 @@ const fileNameLength = 20;
  * State manager for the extension. Determines if user is logged in and can access features like submitting runnable solutions
  */
 export class Engine {
-  /** @type {LeetCode} */
-  apiEntry;
+    /** @type {LeetCode} */
+    apiEntry;
 
-  /** @type {boolean} Set to true if there are valid credentials for authenticated access. */
-  authenticated;
+    /** @type {boolean} Set to true if there are valid credentials for authenticated access. */
+    authenticated;
 
-  /** @type {vscode.TreeDataProvider} */
-  sidePanelProvider;
+    /** @type {vscode.TreeDataProvider} */
+    sidePanelProvider;
 
-  /** @type {string} Base name of solution source code file. */
-  solutionFile;
+    /** @type {string} Base name of solution source code file. */
+    solutionFile;
 
-  /** @type {import("@leetnotion/leetcode-api").Problem} Queried data for the current problem. */
-  problemData;
+    /** @type {import("@leetnotion/leetcode-api").Problem} Queried data for the current problem. */
+    problemData;
 
-  /** @type {SolutionRunnerProvider} Data provider for the bottom panel view. */
-  panelDataProvider;
+    /** @type {SolutionRunnerProvider} Data provider for the bottom panel view. */
+    panelDataProvider;
 
-  /**
-   *
-   * Construct an engine with optional sign-in credentials
-   *
-   * @param {vscode.Uri} extensionRootUri Root path of the extension.
-   * @param {string} solutionfilename Name of the solution file to look for.
-   * @param {string} [session] Optional LeetCode session id.
-   * @param {string} [csrf] Optional LeetCode csrf token.
-   */
-  constructor(extensionRootUri, solutionfilename, session, csrf) {
-    this.authenticated = false;
-    this.solutionFile = solutionfilename;
+    /**
+     *
+     * Construct an engine with optional sign-in credentials
+     *
+     * @param {vscode.Uri} extensionRootUri Root path of the extension.
+     * @param {string} solutionfilename Name of the solution file to look for.
+     * @param {string} [session] Optional LeetCode session id.
+     * @param {string} [csrf] Optional LeetCode csrf token.
+     */
+    constructor(extensionRootUri, solutionfilename, session, csrf) {
+        this.authenticated = false;
+        this.solutionFile = solutionfilename;
 
-    if (session && csrf) {
-      const cred = new Credential({ csrf: csrf, session: session });
-      this.apiEntry = new LeetCode(cred);
-      this.apiEntry.whoami().then((wai) => (this.authenticated = wai.isSignedIn));
-    } else {
-      this.apiEntry = new LeetCode();
+        if (session && csrf) {
+            const cred = new Credential({ csrf: csrf, session: session });
+            this.apiEntry = new LeetCode(cred);
+            this.apiEntry
+                .whoami()
+                .then((wai) => (this.authenticated = wai.isSignedIn));
+        } else {
+            this.apiEntry = new LeetCode();
+        }
+
+        this.sidePanelProvider = new LeetProblemProvider(this.apiEntry);
+        this.panelDataProvider = new SolutionRunnerProvider(extensionRootUri);
     }
 
-    this.sidePanelProvider = new LeetProblemProvider(this.apiEntry);
-    this.panelDataProvider = new SolutionRunnerProvider(extensionRootUri);
-  }
+    /**
+     * Command handler for importing problems
+     *
+     * @param {LeetHeading} heading
+     */
+    importProblem(heading) {
+        const slug = heading.getSlug();
+        console.log("import commanded for", slug);
 
-  /**
-   * Command handler for importing problems
-   *
-   * @param {LeetHeading} heading
-   */
-  importProblem(heading) {
-    const slug = heading.getSlug();
-    console.log("import commanded for", slug);
-
-    this.apiEntry
-      .problem(slug)
-      .then((p) => (this.problemData = p))
-      .catch((err) =>
-        vscode.window.showErrorMessage(
-          `Error while loading '${heading.problemData.title}': ${err}`,
-        ),
-      )
-      .then(this.createSolutionFile.bind(this));
-  }
-
-  /**
-   * Getter for side panel data provider.
-   */
-  getSidePanelProvider() {
-    return this.sidePanelProvider;
-  }
-
-  /**
-   * Attempt to create a solution file in the current workspace folder.
-   *
-   * @return {Promise<void>}
-   */
-  async createSolutionFile() {
-    if (!vscode.workspace.workspaceFolders || vscode.workspace.workspaceFolders.length === 0) {
-      vscode.window.showErrorMessage("No workspace is open");
-      return;
+        this.apiEntry
+            .problem(slug)
+            .then((p) => (this.problemData = p))
+            .catch((err) =>
+                vscode.window.showErrorMessage(
+                    `Error while loading '${heading.problemData.title}': ${err}`,
+                ),
+            )
+            .then(this.createSolutionFile.bind(this));
     }
 
-    // per language configuration can be done here
-    const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-    const solutionPath = path.join(workspacePath, `${clampFileName(this.problemData)}.js`);
-    const selectedLanguage = this.problemData.codeSnippets.filter((cs) => cs.lang == "JavaScript");
-    const content = selectedLanguage[0].code;
-
-    try {
-      await fs.promises.writeFile(solutionPath, content);
-      vscode.window.showInformationMessage("Solution file created");
-    } catch (error) {
-      vscode.window.showErrorMessage(`Error creating solution file: ${error.message}`);
+    /**
+     * Getter for side panel data provider.
+     */
+    getSidePanelProvider() {
+        return this.sidePanelProvider;
     }
-  }
 
-  getPanelProvider() {
-    return this.panelDataProvider;
-  }
+    /**
+     * Attempt to create a solution file in the current workspace folder.
+     *
+     * @return {Promise<void>}
+     */
+    async createSolutionFile() {
+        if (
+            !vscode.workspace.workspaceFolders ||
+            vscode.workspace.workspaceFolders.length === 0
+        ) {
+            vscode.window.showErrorMessage("No workspace is open");
+            return;
+        }
 
-  /**
-   * Tells the vscode panel to update the content in the solution runner.
-   *
-   * @param {import("@leetnotion/leetcode-api").Problem} problem Problem data queried from Leetcode
-   */
-  sendPanelData(problem) {
-    this.panelDataProvider.updateContents(problem);
-  }
+        // per language configuration can be done here
+        const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+        const problemPath = path.join(
+            workspacePath,
+            clampFileName(this.problemData),
+        );
+
+        try {
+            if (!fs.existsSync(problemPath)) {
+                fs.mkdirSync(problemPath);
+            }
+        } catch (error) {
+            vscode.window.showErrorMessage(
+                `Error creating ${clampFileName(this.problemData)} folder: ${error.message},`,
+            );
+        }
+        const solutionPath = path.join(problemPath, "solution.js");
+        const selectedLanguage = this.problemData.codeSnippets.filter(
+            (cs) => cs.lang == "JavaScript",
+        );
+        const content = selectedLanguage[0].code;
+
+        try {
+            await fs.promises.writeFile(solutionPath, content);
+            vscode.window.showInformationMessage("Solution file created");
+        } catch (error) {
+            vscode.window.showErrorMessage(
+                `Error creating solution file: ${error.message}`,
+            );
+        }
+    }
+
+    getPanelProvider() {
+        return this.panelDataProvider;
+    }
+
+    /**
+     * Tells the vscode panel to update the content in the solution runner.
+     *
+     * @param {import("@leetnotion/leetcode-api").Problem} problem Problem data queried from Leetcode
+     */
+    sendPanelData(problem) {
+        this.panelDataProvider.updateContents(problem);
+    }
 }
 
 function clampFileName(problem) {
-  return problem.title.slice(0, fileNameLength).replace(" ", "") + problem.questionFrontendId;
+    return (
+        problem.title.slice(0, fileNameLength).replace(" ", "") +
+        problem.questionFrontendId
+    );
 }
